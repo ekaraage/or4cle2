a5d1f97b58dac2f66394dcdbdd4e089bb6a0ea0f
ekaraage <u.takuml10@gmail.com>
2025-05-30 17:51:10 +0900

    Submissionにvalidationを追加

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class Submission < ApplicationRecord
-  belongs_to :song
+  belongs_to :song, :user
   belongs_to :user
+
+  validates :user, presence: true
+  validates :song, presence: true
+  # 表示名なのだが、20文字くらいあれば大丈夫な気がする。バックエンドではうるさいこと言っていないので後で変えてもよい
+  validates :screen_name, presence: true, length: { maximum: 20 }
+  validates :score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # こいつは適当に140に設定しているが、別になんでもいい気もする。
+  validates :comment, length: { maximum: 140 }
 end

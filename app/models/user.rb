7e3bcef02abc9a03658073fc412ada1b756b64c5
ekaraage <u.takuml10@gmail.com>
2025-05-31 01:56:54 +0900

    アカウント周りをとりあえず作った

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable, :timeoutable, authentication_keys: [:name]
 
-  validates :name, uniqueness: true, presence: true, length: { maximum: 30 }
+  validates :name, uniqueness: true, presence: true, length: { maximum: 30 },
+                   format: { with: /\A[a-zA-Z0-9_]+\z/, message: 'は英数字とアンダースコアのみ使用できます。' }
   validates :password, complex_password: true
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end

440fba977dd8e2eb862b9104826df00a6883d136
ekaraage <u.takuml10@gmail.com>
2025-05-30 17:59:25 +0900

    songにvalidationを追加

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,9 @@
 
 class Song < ApplicationRecord
   belongs_to :ranking
+
+  validates :ranking, presence: true
+  validates :title, presence: true
+  # これも適当に30文字にしている。バックエンドは何もしてないので後で変えること可能
+  validates :model, presence: true, length: { maximum: 30 }
 end
